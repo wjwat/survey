a67565a2c4903ddc1eb2f8e337390691398f5748
William Watkins <wjwat@onionslice.org>
2022-01-12 17:49:24 -0600

    Add logic to push checkboxes into array
    Co-authored-by: Daniel Lindsey <daniellindsey85254@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,46 @@
 
 $(document).ready(function() {
   $("form#basic-details").on('submit', function(e) {
-    event.preventDefault();
+    e.preventDefault();
 
 
     let isFriend = true;
+		let isNameStupid = true;
 		let isColorStupid = true;
-    let name = $('#friend-name').val();
+
+    let name = $('#friend-name').val().toUpperCase();
     let dob = $('#born').val();
     let telNum = $('#phone-num').val();
     let favColor = $("#color").val();
-		const beverage = $("#beverage").val();
+		let beverage = $("#beverage").val();
 
-    if (telNum.substring(0,5) !== "(602)") {
-      isFriend = false;
-    }
+		var checked = []
+		$("input:checkbox:checked").each(function ()
+		{
+				checked.push($(this).val());
+		});
+		console.log(checked)
+
+		if (name === 'WILLIAM' || name === 'DANIEL') {
+			isNameStupid = false;
+		}
 
 		if (parseInt(dob.substring(0,4)) >= 1990) {
       isFriend = false;
     }
 
+    if (telNum.substring(0,5) !== "(602)") {
+      isFriend = false;
+    }
+
 		if (favColor === '#ffffff' || favColor === '#000000') {
 			isColorStupid = false;
+		}
+
+		if (beverage === "4") {
+			console.log('hydro homie')
+		} else {
+			console.log('enjoy your kidney stones')
 		}
   });
 });
